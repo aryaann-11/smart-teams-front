ada0044e47fea942df55bb349dec8f8404301b39
aryaann-11 <aryansprivatemail@gmail.com>
2022-03-26 14:55:04 +0530

    side bar implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import TasksPage from "./components/pages/tasksPage";
+import SideBar from "./components/sideBar";
 function App() {
   return (
-    <div className="App container">
-      <TasksPage />
+    <div className="App">
+      <div className="container-fluid">
+        <div className="row flex-nowrap">
+          <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+            <SideBar />
+          </div>
+          <div className="col-md-9">
+            <TasksPage />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
